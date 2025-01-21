1b55f44a0a5dbfc1aaec28bf4bc185f6b9c4d063
Ericcurb2923 <81720591+Ericcurb2923@users.noreply.github.com>
2025-01-21 12:36:58 -0600

    Move custom content to customization.js and bugfix

---
+++ customization.js
@@ -0,0 +1,184 @@
+var follow = 'https://www.instagram.com/ericacurb/';
+var chat = 'info@curb.ws';
+var cv = true;
+var download = 'http://curb.ws/ECResumeExtended.pdf';
+var sections = [
+    {
+        color: '#FFFFFFF',
+        theme: 'light',
+        title: 'Westlaw',
+        image: './img/Westlaw.png',
+        imageAccessibilityText: 'Westlaw iOS App',
+        subtitle: 'iOS Apps',
+        //"description": "<p></p>",
+        //"dribbble": "",
+        contentTheme: 'light',
+        blocks: [
+            {
+                background: '#FFFFFF',
+                padding: '0',
+                textColor: '',
+                content: [
+                    './img/Westlaw.png',
+
+                    '<h3>Problem</h3>',
+
+                    '<p>Most legal research is done on desktops, but users seek a mobile experience to stay connected and Thomson Reuters Westlaw, lacked an app that meet the need.</p>',
+                    '<h3>Solution</h3>',
+
+                    '<p>Creating a native app that’s an extension of the Westlaw’s desktop experience is optimal. This will allow legal service companies, law practices, higher education and government employees to stay connected Thomson Reuters Westlaw’s core features from anywhere anytime.</p>',
+                    '<h3>Outcome</h3>',
+
+                    '<ol>',
+                    '<li>Scalable core product features</li>',
+                    '<li>Seven additional feature enhancements</li>',
+                    '<li>15 Awards for Best Legal Research App</li>',
+                    '</ol>',
+                    "<a href='https://www.figma.com/proto/qESLRga2Goq4UJSbxCrygs/Westlaw-CS?page-id=0%3A1&node-id=1-24&p=f&viewport=383%2C445%2C0.25&t=OBAbaTxLmy9QLfuS-1&scaling=min-zoom&content-scaling=fixed'>View Westlaw on iOS case study</a>",
+                ],
+            },
+        ],
+    },
+
+    {
+        color: '#FFFFFFF',
+        theme: 'light',
+        title: 'TaxAct',
+        image: './img/MainT.png',
+        imageAccessibilityText: 'TaxAct',
+        subtitle: 'TaxAct',
+        //"description": "<p></p>",
+        //"dribbble": "",
+        contentTheme: 'light',
+        blocks: [
+            {
+                background: '#FFFFFF',
+                padding: '0',
+                textColor: '',
+                content: [
+                    './img/MainT.png',
+
+                    '<h3>Problem</h3>',
+
+                    '<p>Unclear reasons to buy and inconsistent UI patterns have increased user drop-off, resulting in lower SUS and NPS scores.</p>',
+                    '<h3>Solution</h3>',
+
+                    '<p>Creating a scalable framework and process for the shopping experience while highlighting the product’s value, and minimizing authentication friction.</p>',
+                    '<h3>Outcome</h3>',
+
+                    '<ol>',
+                    '<li>30% increase in performance, accessibility, and SEO</li>',
+                    '<li>Adoption of a mobile first approach and responsive grid system</li>',
+                    '<li>New CMS and reduction of time to market from 3 days to 1 </li>',
+                    '<li>2% increase in start rate goal achieved</li>',
+                    '</ol>',
+                    "<a href='https://www.figma.com/proto/RLE1VHpfSzpxdOOwRtH7lG/TaxAct-Case-Study?page-id=0%3A1&node-id=1-7&viewport=542%2C575%2C0.25&t=8xEcY8agLeF6XQjr-1&scaling=min-zoom&content-scaling=fixed'>View TaxAct case study</a>",
+                ],
+            },
+        ],
+    },
+
+    {
+        color: '#FFFFFF',
+        theme: 'light',
+        title: 'Path.ai',
+        image: './img/Pathai.png',
+        imageAccessibilityText: 'Path.ai',
+        subtitle: 'AI',
+        //"description": "<p></p>",
+        //"dribbble": "",
+        contentTheme: 'light',
+        blocks: [
+            {
+                background: '#FFFFFF',
+                padding: '0',
+                textColor: '',
+                content: [
+                    './img/Pathai.png',
+
+                    '<h3>Problem</h3>',
+
+                    '<p>The traditional retirement planning process can be cumbersome, impersonal, and inefficient. Existing services often fail to consider the unique financial situations and goals of individuals, lacking personalized advice that integrates various financial aspects such as income, expenses, risk tolerance, and longevity planning.</p>',
+                    '<h3>Solution</h3>',
+
+                    "<p>Creating Path.ai, a comprehensive retirement planning service that includes personalized plans based on a user's projected income, expenses, assets, liabilities, and risk tolerance is the goal. Path's AI system provides advice on pension optimization, healthcare planning, social security benefits, and longevity considerations, integrating these with educational resources to foster informed decision-making.</p>",
+                    '<h3>Outcome</h3>',
+
+                    '<ol>',
+                    '<li>AI technology</li>',
+                    '<li>E-Learning Plan</li>',
+                    '</ol>',
+                    "<a href='mailto:info@curb.ws'>Contact for case study</a>",
+                ],
+            },
+        ],
+    },
+    {
+        color: '#FFFFFF',
+        theme: 'light',
+        title: 'Beacon',
+        image: './img/BeaconDS.png',
+        imageAccessibilityText: 'Match Video Chat',
+        subtitle: 'Video Chat',
+        //"description": "<p></p>",
+        //"dribbble": "",
+        contentTheme: 'light',
+        blocks: [
+            {
+                background: '#FFFFFF',
+                padding: '0px',
+                textColor: '',
+                content: [
+                    './img/BeaconDS.png',
+                    '<h3>Problem</h3>',
+
+                    '<p>The current design system lacks guidelines, a cohesive design language, and reusable patterns, hindering product consistency, time to market and limits user experience enhancements.</p>',
+                    '<h3>Solution</h3>',
+
+                    '<p>Producing a design system that unifies visual language, reduces design debt, streamlines the design and development process, and elevates product quality.</p>',
+                    '<h3>Outcome</h3>',
+
+                    '<ol>',
+                    '<li>A thoughtful design language</li>',
+                    '<li>Scalable components and guidelines</li>',
+                    '<li>Elevated product quality</li>',
+                    '</ol>',
+                    "<a href='mailto:info@curb.ws'>Contact for case study</a>",
+                ],
+            },
+        ],
+    },
+
+    {
+        color: '#FFFFFF',
+        theme: 'light',
+        title: 'Ethos',
+        image: './img/EthosCS.png',
+        imageAccessibilityText: 'Ethos Case Study',
+        subtitle: 'Video Chat',
+        //"description": "<p></p>",
+        //"dribbble": "",
+        contentTheme: 'light',
+        blocks: [
+            {
+                background: '#FFFFFF',
+                padding: '0px',
+                textColor: '',
+                content: [
+                    './img/EthosCS.png',
+                    '<h3>Problem</h3>',
+
+                    '<p>There’s currently no essential guide or lifestyle website that appeals to clothing, health, podcast, or informative articles that focus on the holistic person.</p>',
+                    '<h3>Solution</h3>',
+
+                    '<p>Creating Ethos as brand that inspires and unites communities through philosophy, culture, health, and lifestyles.</p>',
+                    '<h3>Outcome</h3>',
+
+                    '<p>Qualitative testing In progress.</p>',
+
+                    "<a href='https://www.figma.com/deck/tOUddoMJFGLBbyg1nvqjRc/Ethos-CS?node-id=1-75&node-type=slide&viewport=-178%2C-121%2C1&t=E1rfjoSG68nEGDNh-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1'>View Ethos case study</a>",
+                ],
+            },
+        ],
+    },
+];
