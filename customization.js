7a6206242182034467922a6b205d4cbcde95f8fb
Ericcurb2923 <81720591+Ericcurb2923@users.noreply.github.com>
2025-04-02 18:44:35 -0500

    Update customization.js

--- customization.js
+++ customization.js
@@ -11,10 +11,7 @@ const about = `<div>
         I am a versatile designer with extensive experience working alongside exceptional teams and clients on a variety of projects. My skill set includes complete branding solutions, as well as designing for both desktop and mobile applications. I specialize in information architecture, wireframing, prototyping, and the creation of design systems, all informed by comprehensive user research.  </p>
 
 <p>
-</p>
-
-In addition, I have successfully led and managed the UX process within Agile frameworks, collaborating with both onshore and offshore teams on B2B and B2C SaaS initiatives. My methodology combines systems and atomic design with design thinking, utilizing tools like Figma and Adobe’s suite to craft meaningful user experiences.
-    </p>
+In addition, I have successfully led and managed the UX process within Agile frameworks, collaborating with both onshore and offshore teams on B2B and B2C SaaS initiatives. My methodology combines systems and atomic design with design thinking, utilizing tools like Figma and Adobe’s suite to craft meaningful user experiences. </p>
 
     <p>
         <a href="mailto:info@curb.ws">
