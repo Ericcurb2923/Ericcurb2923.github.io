5ac9953353ff08a55bcc24c6362d6b625a0a8341
Ericcurb2923 <81720591+Ericcurb2923@users.noreply.github.com>
2025-07-07 23:35:28 -0500

    Update customization.js

--- customization.js
+++ customization.js
@@ -415,7 +415,7 @@ const projects = [
                     </ol>
 
                     <p>
-                        The iOS apps addressed the needs for legal services companies, law practices, government employees, and educational institutions, through organized access to legal information from Westlaw anywhere. As a result, the Westlaw iPhone app won over 13 awards for the best legal research app. Check out the case study link for an in depth analysis of the project. Also view in App Store.
+                        The iOS apps addressed the needs for legal services companies, law practices, government employees, and educational institutions, through organized access to legal information from Westlaw anywhere. As a result, the Westlaw iPhone app won over 13 awards for the best legal research app. View case study link above for an in depth analysis of the project. Also view in App Store.
                     </p>
 
                     <img class="desktop-only" src="./img/WestlawFMD.png">
